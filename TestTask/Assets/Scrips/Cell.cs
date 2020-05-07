4eaa6ac3e597e080e5ed605108a049ddea694bab
Fox747 <rus747@gmail.com>
2020-05-07 21:49:40 +0300

    step two

--- TestTask/Assets/Scrips/Cell.cs
+++ TestTask/Assets/Scrips/Cell.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
+    private Net net;
+    public float CellScale;
     private void SetSymbol()
     {
 
@@ -14,10 +17,21 @@ public class Cell : MonoBehaviour
 
     }
     
-    
+    private void CellDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+    private void OnDestroy()
+    {
+        this.net.RestartEvent -= this.CellDestroy;
+    }
+
     void Start()
     {
-        SetSymbol();
+        this.net = FindObjectOfType<Net>();
+        this.net.RestartEvent += this.CellDestroy;
+        this.SetSymbol();
+       // GetComponent<RectTransform>().localScale = new Vector2(CellScale, CellScale);
     }
 
 }
--- TestTask/Assets/Scrips/Net.cs
+++ TestTask/Assets/Scrips/Net.cs
@@ -1,59 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 
 public class Net : MonoBehaviour
 {
+    public delegate void Restart();
+    public event Restart RestartEvent;
+    public float cellScale;
+
     public void CellsMaker(float x, float y, float scaleSymbol)
     {
         Canvas canvas = FindObjectOfType<Canvas>();
         var canvasTransform = canvas.GetComponent<Transform>();
         GameObject Cell = new GameObject("Cell");
+        Cell.AddComponent<Cell>();
         Font fntCell = Resources.Load<Font>("Font/fntCell");
         Cell.AddComponent<Text>();
         var cellTxt = Cell.GetComponent<Text>();
         cellTxt.font = fntCell;
         cellTxt.fontSize = 50;
         cellTxt.color = Color.white;
-        cellTxt.text = "C";
-        Cell.AddComponent<Cell>();
+        cellTxt.alignment = TextAnchor.MiddleCenter;
+       
+        
         var cellTransform = Cell.GetComponent<RectTransform>();
         cellTransform.SetParent(canvasTransform);
         cellTransform.anchorMax = new Vector2(0, 1);
         cellTransform.anchorMin = new Vector2(0, 1);
+        cellTransform.localScale = new Vector2( scaleSymbol, scaleSymbol);
         Cell.transform.localPosition = new Vector2(x, y);
+
+        ContentManager contentManager = FindObjectOfType<ContentManager>();
+        cellTxt.text = contentManager.content[Random.Range(0, contentManager.content.Length)];
     }
 
     public void CellsGenerate()
     {
+        if(GameObject.Find("Cell") != null)
+        {
+            RestartEvent();// Событие на удаление ранее созданных экземпляров Cell 
+        }
+        
         GameManager gameManager = FindObjectOfType<GameManager>();
         if ((gameManager.numX != null) && (gameManager.numX != null))
         {
             this.CellSetuper(gameManager.fieldSize.x, gameManager.fieldSize.y, gameManager.numX, gameManager.numY);
         }
-        
     }
 
     public void CellSetuper( float fieldSizeX, float fieldSizeY, int numX, int numY)
     {
-        float pointZeroX = - Screen.width / 2 + 150;
-        float pointZeroY = 200;
-        float stepX = 50;
-        float stepY = 70;
-        int numCells = numX * numY;
+        float pointZeroX = -(fieldSizeX / numX + 1) * (numX - 1) / 2; // координата Х левой верхней ячейки ( левая граница поля)
+        float pointZeroY = 180 + fieldSizeY / 2;// координата Y левой верхней ячейки 
+        float stepX = fieldSizeX / numX + 1;// назначаем шаг по горизонтали
+        Debug.Log("numX = " + numX);
+        float stepY = fieldSizeY / numY + 1;// назначаем шаг по вертикали
+        this.cellScale = 1; // / numX;
         for (int i = 0; i < numY; i++)
         {
             for (int j = 0; j < numX; j++)
             {
-                this.CellsMaker(pointZeroX + stepX * j, pointZeroY + stepY * i , 1);
+                this.CellsMaker(pointZeroX + stepX * j, pointZeroY - stepY * i , cellScale);
             }
                 
         }
     }
-    //void Start()
-    //{
-         
-    //}
 }
